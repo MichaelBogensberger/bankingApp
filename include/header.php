124db28d357bf2102b049333d89c27653729c6b7
MichaelBogensberger <bogensbergermichael@gmail.com>
2021-11-08 14:29:16 +0100

    v2

---
+++ include/header.php
@@ -0,0 +1,9 @@
+<!-- Halfmoon CSS -->
+<link href="https://cdn.jsdelivr.net/npm/halfmoon@1.1.1/css/halfmoon-variables.min.css" rel="stylesheet" />
+
+<!-- Halfmoon JS -->
+<script src="https://cdn.jsdelivr.net/npm/halfmoon@1.1.1/js/halfmoon.min.js"></script>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link rel="stylesheet" href="include/style.css">
--- index.php
+++ index.php
@@ -3,13 +3,10 @@
 <head>
 <title>Page Title</title>
 
-<!-- Halfmoon CSS -->
-<link href="https://cdn.jsdelivr.net/npm/halfmoon@1.1.1/css/halfmoon-variables.min.css" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
-<!-- Halfmoon JS -->
-<script src="https://cdn.jsdelivr.net/npm/halfmoon@1.1.1/js/halfmoon.min.js"></script>
 
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php include 'include/header.php';?>
 
 
 
@@ -21,6 +18,7 @@
     data-sidebar-type="overlayed-sm-and-down">
     <!-- Sticky alerts (toasts), empty container -->
     <div class="sticky-alerts"></div>
+
     <!-- Navbar -->
     <nav class="navbar">
      
@@ -73,41 +71,164 @@
           </div>
         </div>
       </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     </nav>
 
     
 
+
+
+
+<!-- Sidebar overlay -->
+<div class="sidebar-overlay" onclick="halfmoon.toggleSidebar()"></div>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <img src="https://eu.ui-avatars.com/api/?name=Julian+Meilinger&size=90" alt="pfp" class="pfp-dash rounded-circle mx-auto d-block">
+      <h5 class="text-center">Julian Meilinger</h5>
+
+
+      <li class="sidebar-link sidebar-link-with-icon">
+        <span class="sidebar-icon">
+        <i class="material-icons">cake</i>
+        </span>
+        11.06.2001
+    </li>
+
+    <li class="sidebar-link sidebar-link-with-icon">
+        <span class="sidebar-icon">
+        <i class="material-icons">home</i>
+        </span>
+        Hinterseberweg 12
+    </li>
+
+    <li class="sidebar-link sidebar-link-with-icon">
+        <span class="sidebar-icon">
+        <i class="material-icons">location_city</i>
+        </span>
+        Imst 6460
+    </li>
+
+    <li class="sidebar-link sidebar-link-with-icon">
+        <span class="sidebar-icon">
+        <i class="material-icons">flag</i>
+        </span>
+        Österreich
+    </li>
+
+
     </div>
+
+
+
+
     <!-- Content wrapper -->
     <div class="content-wrapper">
-      ...
+      
+    
+
+
+    <div class="row">
+      <div class="col-6">
+        
+      <div class="w-400 mw-full"> <!-- w-400 = width: 40rem (400px), mw-full = max-width: 100% -->
+        <div class="card">
+
+          <li class="sidebar-link sidebar-link-with-icon gh-title">
+            <span class="sidebar-icon gh-span">
+            <i class="material-icons">attach_money</i>
+            </span>
+            Guthaben
+          </li>
+
+          <p class="text-muted">
+           Unter diesem Reiter finden Sie Ihr aktuelles Guthaben über folgendes Konto.
+          </p>
+
+        <dl class="row">
+          <dt class="col-lg-10">Aktueller Wert:</dt>
+          <dd class="col-lg-2">120€</dd>
+        </dl>
+
+        </div>
+      </div>
 
 
 
+
+      <div class="w-400 mw-full"> <!-- w-400 = width: 40rem (400px), mw-full = max-width: 100% -->
+        <div class="card">
+
+          <li class="sidebar-link sidebar-link-with-icon gh-title">
+            <span class="sidebar-icon gh-span">
+            <i class="material-icons">info</i>
+            </span>
+            Bankkonto
+          </li>
+
+          <p class="text-muted">
+           Hier finden Sie Informationen über Ihr Bankkonto wie zb. IBAN oder BIC.
+          </p>
+
+        <dl class="row iban-row">
+          <dt class="col-lg-4">IABN:</dt>
+          <dd class="col-lg-8">AT35 3500 0404 2343 2343</dd>
+        </dl>
+
+        <dl class="row bic-row">
+          <dt class="col-lg-4">BIC:</dt>
+          <dd class="col-lg-8">RSV12354334</dd>
+        </dl>
+
+
+        </div>
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+      </div>
+      <div class="col-6">
+        2 of 3 (wider)
+      </div>
+    </div>
+
+
+
+
+      
 
 
     </div>
+
+
+
+
+
     <!-- Navbar fixed bottom -->
     <nav class="navbar navbar-fixed-bottom">
-      ...
+      Michael Bogensberger, Niklas Heim
     </nav>
-</div>
 
+
+
+
+  </div>
+
+
+
+
+
+
+
+
+  
 
 
 
