05c125e3e41bfaf4ba3cf077aaff20849b09c29c
niklasheim <niklasheim@gmx.at>
2022-02-01 09:47:55 +0100

    register val

--- register.php
+++ register.php
@@ -2,6 +2,14 @@
     include './include/database.php';
     include './include/fun.php';
 
+    function val($string){
+        if (!preg_match('/[^A-Za-z0-9]/', $string)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     $vorname = $_POST["vorname"];
     $nachname = $_POST["nachname"];
     $geburtsdatum = $_POST["geburtsdatum"];
@@ -12,15 +20,22 @@
     $plz = $_POST["plz"];
     $password = $_POST["password"];
 
-    $iban = "AT 38 " . rand(1000, 9999) . " " . rand(1000, 9999) . " " . rand(1000, 9999) . " " . rand(1000, 9999);
-    $bic = "AGENAT69";
+    if(val($vorname) && val($nachname) && val($username) && val($land) && val($stadt) && val($plz) && val($strasse) && strlen($password) > 3){
+        $iban = "AT 38 " . rand(1000, 9999) . " " . rand(1000, 9999) . " " . rand(1000, 9999) . " " . rand(1000, 9999);
+        $bic = "AGENAT69";
 
-    $db = connect();
+        $db = connect();
 
-    $sql = "INSERT INTO user (vorname, nachname, geburtsdatum, username, strasse, land, stadt, plz, guthaben, password, iban, bic) 
-            VALUES ('$vorname', '$nachname', '$geburtsdatum', '$username', '$strasse', '$land', '$stadt', $plz, 0, '$password', '$iban', '$bic')";
-    // use exec() because no results are returned
-    $db->exec($sql);
+        $sql = "INSERT INTO user (vorname, nachname, geburtsdatum, username, strasse, land, stadt, plz, guthaben, password, iban, bic) 
+                VALUES ('$vorname', '$nachname', '$geburtsdatum', '$username', '$strasse', '$land', '$stadt', $plz, 0, '$password', '$iban', '$bic')";
+        // use exec() because no results are returned
+        $db->exec($sql);
 
-    header('Location: ./einloggen.php');
+        header('Location: ./einloggen.php');
+    } else {
+        header('Location: ./registrieren.php');
+    }
+
+
+    
 ?>
